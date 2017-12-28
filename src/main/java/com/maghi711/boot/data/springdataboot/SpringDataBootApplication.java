f344a03404bc10c6627c069a9181a9102c81e536
Mahesh J <maghi711@gmail.com>
2017-12-28 09:52:00 +0530

    Initial Commit

---
+++ src/main/java/com/maghi711/boot/data/springdataboot/SpringDataBootApplication.java
@@ -0,0 +1,12 @@
+package com.maghi711.boot.data.springdataboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataBootApplication.class, args);
+	}
+}
